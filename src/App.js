ad98dcb9e1aee78dbe3d1ac49f72fe6af53695ea
Camilo Pinzon <pinzonac@gmail.com>
2022-03-29 13:23:15 -0500

    Scaffolding capstone project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 const  App = () => {
   return (
-    <div>
-      Hello World!
+    <div className="categories-container">
+      <div className="category-container">
+        <img src="" alt="" />
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" alt="" />
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" alt="" />
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" alt="" />
+        <div className="category-body-container">
+          <h2>Womwen</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" alt="" />
+        <div className="category-body-container">
+          <h2>Men</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
