951c72104a1790f3f25d0655ad6db3f39d6c1676
Camilo Pinzon <pinzonac@gmail.com>
2022-04-04 10:05:39 -0500

    Stiling navigation and logo

--- src/routes/navigaton/navigation.component.jsx
+++ src/routes/navigaton/navigation.component.jsx
@@ -1,12 +1,16 @@
 import { Fragment } from 'react';
 import {Outlet, Link} from 'react-router-dom';
 
+import { ReactComponent as CrwnLogo } from '../../assets/svg/crown.svg';
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
         <Fragment>
             <nav className='navigation'>
                 <Link className="logo-container" to="/">
-                    <div className="logo">LOGO</div>
+                    <CrwnLogo className='logo' />
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to='shop'>SHOP</Link>
