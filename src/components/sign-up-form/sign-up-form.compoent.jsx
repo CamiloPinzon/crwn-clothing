83ec31c6d55bf7c08824f91216a8b10e28cde3ac
Camilo Pinzon <pinzonac@gmail.com>
2022-04-05 12:43:05 -0500

    default button componenet

--- src/components/sign-up-form/sign-up-form.compoent.jsx
+++ src/components/sign-up-form/sign-up-form.compoent.jsx
@@ -1,7 +1,10 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
     displayName: '',
@@ -45,8 +48,9 @@ const SignUpForm = () => {
     }
 
     return (
-        <Fragment>
-            <h1>Sign up with your email and password</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput
                     label='Display name'
@@ -97,9 +101,9 @@ const SignUpForm = () => {
                         }
                     }/>
 
-                <button type="submit">Sign Up</button>
+                <Button type="submit" >Sign Up</Button>
             </form>
-        </Fragment>
+        </div>
     )
 }
 
